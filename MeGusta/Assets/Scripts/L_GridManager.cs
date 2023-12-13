0c0f3d071719f9540581b8ae555b90792a18aed9
Liron9906 <lironplaysmc123@gmail.com>
2023-12-13 14:45:10 +0200

    working basic tile system PogChamp

--- MeGusta/Assets/Scripts/L_GridManager.cs
+++ MeGusta/Assets/Scripts/L_GridManager.cs
@@ -4,4 +4,18 @@ using UnityEngine;
 
 public class L_GridManager : MonoBehaviour
 {
+	[SerializeField] GameObject[] Tarr;
+	private void Start()
+	{
+		Tiles[] tiles = new Tiles[Tarr.Length];
+
+		for (int i = 0; i < Tarr.Length; i++)
+		{
+			for(int j = 0;j<=i;j++)
+			{
+				tiles[i]=new Tiles(Tarr[i],i,j);
+				Debug.Log(tiles[i].ToString());
+			}
+		}
+	}
 }
--- MeGusta/Assets/Scripts/Tiles.cs
+++ MeGusta/Assets/Scripts/Tiles.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Tiles : MonoBehaviour
 {
-	
-	bool IsOcupied;
-	float positionx;
-	float positiony;
-	public Tiles()
+	protected GameObject empty;
+	protected bool IsOccupied;
+	protected float positionx;
+	protected float positiony;
+	public Tiles(GameObject empty,float positionx,float positiony)
 	{
-	
+		this.empty = empty;
+		this.IsOccupied = false;
+		this.positionx = positionx;	
+		this.positiony = positiony;
 	}
-	
+	public bool GetIsOccupied() {  return this.IsOccupied; }
+	public float GetPositionx() {  return this.positionx; }
+	public float GetPositiony() {  return this.positiony; }
+	public void SetPositionx(float x) {  this.positionx = x; }
+	public void SetPositiony(float y) {  this.positiony = y; }
+	public void SetIsOccupied() {  this.IsOccupied=!this.IsOccupied;Debug.Log(this.IsOccupied); }
+	public override string ToString()
+	{
+		return $"My Position is- ({this.GetPositionx()},{this.GetPositiony()})";
+	}
 }
+
