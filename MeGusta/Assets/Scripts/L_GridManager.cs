a25380b7c9fed3f3c0ad50f7a22c459177ab5d12
Liron9906 <lironplaysmc123@gmail.com>
2023-12-13 12:38:30 +0200

    noice
    imgoingtokms

---
+++ MeGusta/Assets/Scripts/L_GridManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L_GridManager : MonoBehaviour
+{
+	public GameObject[] oTiles;
+
+	private void OnMouseDown()
+	{
+		for (int i = 0; i < oTiles.Length; i++)
+		{
+
+		}
+	}
+}
