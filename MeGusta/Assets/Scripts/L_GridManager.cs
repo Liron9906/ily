d906d012ae19731b98cb02c5f062d4aa0365252f
Liron9906 <lironplaysmc123@gmail.com>
2023-12-13 15:46:08 +0200

    +more

--- MeGusta/Assets/Scripts/L_GridManager.cs
+++ MeGusta/Assets/Scripts/L_GridManager.cs
@@ -18,4 +18,5 @@ public class L_GridManager : MonoBehaviour
 			}
 		}
 	}
+
 }
---
+++ MeGusta/Assets/Scripts/L_PrefabScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class L_PrefabScript : MonoBehaviour
+{
+	private void Update()
+	{
+		
+	}
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Throwable")
+		{
+
+		}
+	}
+}
