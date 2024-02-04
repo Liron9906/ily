9521f629c09d6cfd388d29280c42b5bb3b58db62
Katzzo <yiftachziv90@gmail.com>
2024-02-04 15:58:25 +0200

    early UI

--- MeGusta/Assets/Scripts/Y_LeftUI.cs
+++ MeGusta/Assets/Scripts/Y_LeftUI.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Y_LeftUI : MonoBehaviour
 {
-    [SerializeField] Scrollbar selectorUI;
+    [SerializeField] Slider selectorUI;
+    //[SerializeField] Scrollbar selectorUI;
+    [SerializeField] GameObject[] curTiles= new GameObject[4];
+    public GameObject curTile;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +18,28 @@ public class Y_LeftUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonUp("Fire2") && 0 < selectorUI.value)
+        if (Input.GetButtonUp("Fire2"))
         {
-            selectorUI.value = (selectorUI.value - 0.25f);
+            selectorUI.value -= 1;
         }
-        if (Input.GetButtonUp("Fire3") && selectorUI.value < 1)
+        if (Input.GetButtonUp("Fire3"))
         {
-            selectorUI.value += 0.25f;
+            selectorUI.value += 1;
+        }
+        switch (selectorUI.value)
+        {
+            case 1:
+                curTile = curTiles[0];
+                break;
+            case 2:
+                curTile = curTiles[1];
+                break;
+            case 3:
+                curTile = curTiles[2];
+                break;
+            case 4:
+                curTile = curTiles[3];
+                break;
         }
     }
 }
