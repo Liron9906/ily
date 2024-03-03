a166d5b02b025aaae7a5629c979602ee6365e1bc
Katzzo <yiftachziv90@gmail.com>
2024-03-03 15:21:36 +0200

    plus and wind start
    :100:

---
+++ MeGusta/Assets/Scripts/WINDMANAGEr.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WINDMANAGEr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool isWind = true;
+    float timer;
+    void Start()
+    {
+        timer = Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer+3<Time.deltaTime)
+        {
+            isWind = true;
+            GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity+new Vector2(-30,0);
+            timer = Time.deltaTime;
+            isWind = false;
+        }
+    }
+}
