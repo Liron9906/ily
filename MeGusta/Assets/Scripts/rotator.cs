990e6fc0f6abfdd1a56880035e141c9902537254
Katzzo <yiftachziv90@gmail.com>
2024-03-27 15:07:00 +0200

    more skillcheck

--- MeGusta/Assets/Scripts/rotator.cs
+++ MeGusta/Assets/Scripts/rotator.cs
@@ -6,6 +6,7 @@ public class rotator : MonoBehaviour
 {
     [SerializeField] int speed = 100;
     [SerializeField] GameObject pivot;
+    float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,14 @@ public class rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer = Time.deltaTime;
         if (FindObjectOfType<skillCheck>().stopSpin == false)
         {
             transform.RotateAround(pivot.transform.position, new Vector3(0, 0, 1), speed * Time.deltaTime);
+        }
+        else if (timer + 3 < Time.deltaTime)
+        {
+            FindObjectOfType<skillCheck>().stopSpin = true;
         }
     }
 }
--- MeGusta/Assets/Scripts/skillCheck.cs
+++ MeGusta/Assets/Scripts/skillCheck.cs
@@ -11,6 +11,18 @@ public class skillCheck : MonoBehaviour
     public bool didHit;
     public static int countOfHits;
     [SerializeField] GameObject taskCanvas;
+    [SerializeField] GameObject circle;
+    [SerializeField] GameObject square;
+    [SerializeField] GameObject empty;
+    [SerializeField] GameObject big;
+    private void Awake()
+    {
+        StartCoroutine(cage.fadeInAndOut(circle, true, 3));
+        StartCoroutine(cage.fadeInAndOut(square, true, 3));
+        StartCoroutine(cage.fadeInAndOut(empty, true, 3));
+        StartCoroutine(cage.fadeInAndOut(big, true, 3));
+
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -22,21 +34,21 @@ public class skillCheck : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             endSC = true;
-            stopSpin = true;
-            if (saveStat=="good")
+            //stopSpin = true;
+            if (saveStat == "good")
             {
                 didHit = true;
                 countOfHits++;
                 //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
 
-            else if (saveStat=="bad")
+            else if (saveStat == "bad")
             {
                 didHit = true;
                 countOfHits = 0;
                 //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
-            Destroy(taskCanvas, 2);
+            //Destroy(taskCanvas, 2);
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
