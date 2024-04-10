a791784590957d8d2c8f192ab14546215a854e39
Katzzo <yiftachziv90@gmail.com>
2024-04-10 17:11:04 +0300

    perchance

--- MeGusta/Assets/Scripts/Shape.cs
+++ MeGusta/Assets/Scripts/Shape.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Presets;
+//using UnityEditor.Presets;
 using UnityEngine;
 using UnityEngine.Rendering;
 
